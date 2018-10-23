6b8f0e005f2b899bed26dbcc333e74d0e80d8690
jp8874 <jp8874@studnet.uni-lj.si>
2018-10-23 15:16:06 +0200

    Revert "Merge branch 'master' of https://github.com/fri-rso-2018-skupina-1-teamlj/rso-teamlj-users"
    This reverts commit 3954a03446149c42229053b5aa8d045f65105465, reversing
    changes made to 2f3ca1577a6b58f35f7020202f672159ce67e9c7.
    
    Reverting the merge...

--- Dockerfile
+++ Dockerfile
@@ -6,6 +6,6 @@ WORKDIR /app
 
 ADD ./api/target/users-api-1.0.0-SNAPSHOT.jar /app
 
-EXPOSE 8080
+EXPOSE 8081
 
 CMD java -jar users-api-1.0.0-SNAPSHOT.jar
--- api/src/main/java/si/fri/rso/teamlj/rents/api/v1/RentsApplication.java
+++
@@ -1,8 +0,0 @@
-package si.fri.teamlj.rents.orders.api.v1;
-
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-
-@ApplicationPath("/v1")
-public class RentsApplication extends Application {
-}
