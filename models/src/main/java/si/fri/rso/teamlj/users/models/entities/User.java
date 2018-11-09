1249b78702de779957285e89366636c18cceab03
jp8874 <jp8874@studnet.uni-lj.si>
2018-11-09 20:35:04 +0100

    Uporabniku dodano polje inUse

--- models/src/main/java/si/fri/rso/teamlj/users/models/entities/User.java
+++ models/src/main/java/si/fri/rso/teamlj/users/models/entities/User.java
@@ -26,6 +26,8 @@ public class User {
 
     private String address;
 
+    private boolean inUse;
+
     @Column(name = "date_of_birth")
     private Instant dateOfBirth;
 
@@ -63,6 +65,15 @@ public class User {
     public void setAddress(String address) {
         this.address = address;
     }
+
+    public boolean getInUse() {
+        return inUse;
+    }
+
+    public void setInUse(boolean inUse) {
+        this.inUse = inUse;
+    }
+
 
     public Instant getDateOfBirth() {
         return dateOfBirth;
